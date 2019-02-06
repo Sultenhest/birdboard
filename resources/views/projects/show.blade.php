a092efd3259c7b32b36cf46a6663f84da5645383
Simon Konstantyner <simon@konstantyner.dk>
2019-02-06 17:10:06 +0100

    episode 11

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-    <a href="/projects">Go Back</a>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-grey text-sm font-normal">
+                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+            </p>
+            <a href="/projects/create"  class="button">New Project</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-grey text-lg font-normal mb-3">Tasks</h2>
+
+                    <div class="card mb-3">Lorem Ipsum.</div>
+                    <div class="card mb-3">Lorem Ipsum.</div>
+                    <div class="card mb-3">Lorem Ipsum.</div>
+                    <div class="card mb-3">Lorem Ipsum.</div>
+                    <div class="card">Lorem Ipsum.</div>
+                </div>
+                
+                <div>
+                    <h2 class="text-grey text-lg font-normal mb-3">General Notes</h2>
+
+                    <textarea class="card w-full" style="min-height:200px;">Lorem Ipsum.</textarea>
+                </div>
+            </div>
+
+            <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+            </div>
+        </div>
+    </main>
 @endsection
